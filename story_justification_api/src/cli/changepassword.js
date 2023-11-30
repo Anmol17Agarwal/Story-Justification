85c53a60a9e1461083711621364bcbf446c9081b
Amogh Madan <amoghmadaan@gmail.com>
2023-11-30 17:03:04 +0530

    Add change password command.

--- story_justification_api/src/cli/changepassword.js
+++ story_justification_api/src/cli/changepassword.js
@@ -1,6 +1,22 @@
+import mongoose from 'mongoose';
+
+import {User} from '../models';
+import {MONGODB_URI} from '../settings';
+import {userValidator} from '../validators';
+
 /**
  * Change password in CLI.
  * @param {string} email
  * @param {string} password
  */
-export default async function changePassword(email, password) {}
+export default async function changePassword(email, password) {
+  const args = {email, password};
+  const validatedData = await userValidator.login.validateAsync(args);
+  await mongoose.connect(MONGODB_URI);
+  const newUser = await User.findOneAndUpdate(
+      {email: validatedData.email},
+      {password: validatedData.password},
+  );
+  await newUser.save();
+  console.info('Password Changed Successfully!');
+}
