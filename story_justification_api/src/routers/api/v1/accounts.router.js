3a20ec7492edbe8682c189509158e4aafdfbb474
Amogh Madan <amoghmadaan@gmail.com>
2023-11-30 17:03:23 +0530

    Fix methods mapped on the router.

--- story_justification_api/src/routers/api/v1/accounts.router.js
+++ story_justification_api/src/routers/api/v1/accounts.router.js
@@ -5,7 +5,7 @@ import {accountController} from '../../../controllers';
 // eslint-disable-next-line new-cap
 const accountRouter = Router();
 accountRouter.route('/login').post(accountController.login);
-accountRouter.route('/detail').post(accountController.detail);
-accountRouter.route('/logout').post(accountController.logout);
+accountRouter.route('/detail').get(accountController.detail);
+accountRouter.route('/logout').delete(accountController.logout);
 
 export default accountRouter;
